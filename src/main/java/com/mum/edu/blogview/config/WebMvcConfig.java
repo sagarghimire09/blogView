c952bab4cf94797ee08bbaa076e0e9607eff1254
Krishna K.C <krishnakc2339@gmail.com>
2019-08-11 17:21:56 -0500

    package path update

--- src/main/java/com/mum/edu/blogview/config/WebMvcConfig.java
+++ src/main/java/com/mum/edu/blogview/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.edu.mum.blogView.config;
+package com.edu.mum.blogview.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
