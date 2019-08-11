7807c43207039fbd803e872f286afd3c5a97812d
krishnakc239 <krishnakc2339@gmail.com>
2019-08-11 17:46:20 -0500

    olde files deleted

--- src/main/java/com/mum/edu/blogview/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edu.mum.blogview.repository;
-
-import com.edu.mum.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- src/main/java/com/mum/edu/blogview/service/CommentService.java
+++
@@ -1,8 +0,0 @@
-package com.edu.mum.blogview.service;
-
-import com.edu.mum.domain.Comment;
-
-public interface CommentService {
-
-    Comment save(Comment comment);
-}
