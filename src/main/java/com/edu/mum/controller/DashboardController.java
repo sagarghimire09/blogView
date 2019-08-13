30ba4d76e9a8e36d5081cea18a8570169782cd78
sagarghimire09 <sagarghimire699@gmail.com>
2019-08-13 16:48:09 -0500

    sidebar attached

---
+++ src/main/java/com/edu/mum/controller/DashboardController.java
@@ -0,0 +1,12 @@
+package com.edu.mum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "views/home/dashboard";
+	}
+}
