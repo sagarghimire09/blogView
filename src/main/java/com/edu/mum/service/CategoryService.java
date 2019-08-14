6f32aae943ad06ef5f9f63dcdb40992a1d546cf5
Saroj Thapa <sarose301@gmail.com>
2019-08-14 10:36:32 -0500

    category incorporated in Post

--- src/main/java/com/edu/mum/service/CategoryService.java
+++ src/main/java/com/edu/mum/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.edu.mum.service;
 import com.edu.mum.domain.Category;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryService {
@@ -10,4 +11,5 @@ public interface CategoryService {
     void saveCategory(Category category);
     void deleteCategory(Long id);
     Optional<Category> getCategoryById(Long id);
+    List<Category> getAllCategory();
 }
--- src/main/java/com/edu/mum/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/edu/mum/service/impl/CategoryServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -36,6 +37,11 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Optional<Category> getCategoryById(Long id) {
         return categoryRepository.findById(id);
+    }
+
+    @Override
+    public List<Category> getAllCategory() {
+        return  categoryRepository.findAll();
     }
 
 
